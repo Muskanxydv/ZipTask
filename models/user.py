a66216b617b4676e69ac78b3a88629a059fabaf1
Levv Foster <muskanyadav4095@gmail.com>
2026-03-13 23:29:39 +0530

    Create User model with attributes and timestamp

---
+++ models/user.py
@@ -0,0 +1,16 @@
+from models import db
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    phone = db.Column(db.String(15), nullable=False)
+
+    password = db.Column(db.String(200), nullable=False)
+
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
