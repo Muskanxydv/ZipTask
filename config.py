3aec093dbbcbf2d3d4c1ba0609f93c664825b3ba
SANA FIRDOS <husainsana642@gmail.com>
2026-04-07 11:33:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
 
 class Config:
-    # Secret key used for sessions/security
     SECRET_KEY = "ziptask_secret_key"
 
-    # SQLite database location
     SQLALCHEMY_DATABASE_URI = "sqlite:///ziptask.db"
 
-    # Disables unnecessary warnings
     SQLALCHEMY_TRACK_MODIFICATIONS = False
